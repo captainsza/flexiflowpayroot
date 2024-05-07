e32ed1f5e1d61082678e2ff62a8adee4552beda7
Zaid Ahmad <100477153+captainsza@users.noreply.github.com>
2024-05-07 19:47:12 +0530

    chore: Update page.tsx to wrap content in a main element

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { ComingSoon } from "@/components/component/comingsoon";
 
 export default function Home() {
   return (
-    <>
+    <main className=" overflow-hidden">
      <ComingSoon/>
-    </>
+    </main>
   );
 }
